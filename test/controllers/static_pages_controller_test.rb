088dd9d8c5171cf717a42296ede1b5fb7e7cf55c
Taka, Toyoshima <oq0q6383qqs@gmail.com>
2019-11-01 19:37:35 +0900

    edit some config

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,13 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test 'should get home' do
+    get root_path
     assert_response :success
   end
 
-  test "should get about" do
-    get static_pages_about_url
+  test 'should get about' do
+    get about_path
     assert_response :success
   end
-
 end
