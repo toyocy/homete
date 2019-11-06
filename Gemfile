c9b6904f517af7d1861c4d3400c797b18fa419dd
Taka, Toyoshima <oq0q6383qqs@gmail.com>
2019-11-06 13:50:11 +0900

    add coverage

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :test do
   gem 'chromedriver-helper'
   gem 'minitest-reporters'
   gem 'minitest'
+  gem 'simplecov', require: false
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,12 @@
 ENV['RAILS_ENV'] ||= 'test'
+if ENV['RAILS_ENV'] == 'test'
+  require 'simplecov'
+  SimpleCov.start 'rails'
+  puts 'required simplecov'
+end
 require_relative '../config/environment'
 require 'rails/test_help'
-require "minitest/reporters"
+require 'minitest/reporters'
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
