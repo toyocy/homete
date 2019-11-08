1858cc66944cd3020d39d6f220e5d9e33031e768
Taka, Toyoshima <oq0q6383qqs@gmail.com>
2019-11-09 00:20:32 +0900

    edit application name

--- db/migrate/20191106050203_create_users.rb
+++ db/migrate/20191106050203_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :email
       t.string :username
+      t.text :introduction
 
       t.timestamps
     end
