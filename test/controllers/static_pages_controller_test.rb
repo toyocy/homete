239331328c49497b76669845a16ff8700aa0e206
Taka, Toyoshima <oq0q6383qqs@gmail.com>
2019-11-18 06:57:37 +0900

    edit layouts

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,19 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = 'Homete'
+  end
+  
   test 'should get home' do
     get root_path
     assert_response :success
+    assert_select 'title', "#{@base_title}".to_s
   end
 
   test 'should get about' do
     get about_path
     assert_response :success
+    assert_select 'title', "About | #{@base_title}"
   end
 end
