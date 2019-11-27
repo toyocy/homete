82b538fdc42b46a347130f371f0d68eece15346e
Taka, Toyoshima <oq0q6383qqs@gmail.com>
2019-11-27 12:54:58 +0900

    implement basic log in and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :posts
   root to: 'static_pages#home'
 
   get 'about', to: 'static_pages#about'
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users
 end
